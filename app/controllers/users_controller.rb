aef7b2efa21ff098e9c418a979b954f7363f6d2c
silence <silence@debianer.(none)>
2010-08-12 18:45:47 +0200

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,24 @@
 class UsersController < ApplicationController
   
   def new
+    @user = User.new
     @title = "Registrieren"
   end
   
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+      flash[:success] = "Registrierung erfolgreich!"
+    else
+      @title = "Registrieren"
+      render 'new'
+    end
   end
 
 end
